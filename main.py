d67c1c3ab2bc5452cbd497a4a227dc0cf10d3bb8
Andrey <andrey@MacBook-Air.local>
2026-01-05 21:51:33 +0500

    fix_logger

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import torch
 from flask import Flask, request, jsonify
-from logger import logger
+import logging
 import whisper
 import uuid
 
@@ -13,21 +13,26 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 MODEL = whisper.load_model("small", device=device)
 ALLOWED_EXTENSIONS = {'mp3'}
 
-logger.info(f"Whisper loaded on device: {device}")
-
 app = Flask(__name__)
 app.config['static'] = STATIC
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+app.logger.info(f"Whisper loaded on device: {device}")
 
 @app.route("/transcription", methods=['POST'])
 def transcription():
     if 'upload-file' not in request.files:
-        logger.warning('No file part')
+        app.logger.warning('No file part')
         return jsonify({'message': 'No file part'}, 400)
 
     audio = request.files['upload-file']
 
     if not audio.filename.endswith('.mp3'):
-        logger.warning('File is not audio')
+        app.logger.warning('File is not audio')
         return jsonify({'message': 'File is not audio'}, 400)
 
     filename = f'{str(uuid.uuid4())}.mp3'
@@ -42,7 +47,7 @@ def transcription():
         return jsonify(result)
     except Exception:
         import traceback
-        logger.error(traceback.format_exc())
+        app.logger.error(traceback.format_exc())
         return jsonify({'message': 'Transcription error'}, 400)
     finally:
         if file_path.exists():
