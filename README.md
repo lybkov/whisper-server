a9fa6207e6cbe9ec89aeb717af1f0bd0b064466c
Andrey <andrey@MacBook-Air.local>
2026-01-06 08:59:19 +0500

    transition to a faster whisper

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Python 3.12
+
 gunicorn --workers 1 --timeout 600 --bind 0.0.0.0:5000 main:app
 apt update && apt install ffmpeg
 
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from pathlib import Path
-
-import torch
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import logging
-import whisper
+from faster_whisper import WhisperModel
 import uuid
 
 STATIC = Path(__file__).resolve().parent / 'static'
 STATIC.mkdir(parents=True, exist_ok=True)
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-MODEL = whisper.load_model("small", device=device)
-ALLOWED_EXTENSIONS = {'mp3'}
+try:
+    MODEL = WhisperModel("turbo", device="cuda", compute_type="float16")
+    device_name = "cuda"
+except Exception:
+    MODEL = WhisperModel("turbo", device="cpu", compute_type="int8")
+    device_name = "cpu"
 
 app = Flask(__name__)
 app.config['static'] = STATIC
@@ -21,34 +22,40 @@ if __name__ != '__main__':
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
-app.logger.info(f"Whisper loaded on device: {device}")
+app.logger.info(f"Whisper loaded on device: {device_name}")
+
 
 @app.route("/transcription", methods=['POST'])
-def transcription():
+def transcription() -> tuple[Response, int] | Response:
     if 'upload-file' not in request.files:
         app.logger.warning('No file part')
-        return jsonify({'message': 'No file part'}, 400)
+        return jsonify({'message': 'No file part'}), 400
 
     audio = request.files['upload-file']
-
     if not audio.filename.endswith('.mp3'):
         app.logger.warning('File is not audio')
-        return jsonify({'message': 'File is not audio'}, 400)
+        return jsonify({'message': 'File is not audio'}), 400
 
-    filename = f'{str(uuid.uuid4())}.mp3'
-
+    filename = f'{uuid.uuid4()}.mp3'
     file_path = STATIC / filename
 
-    audio.save(str(file_path))
-
     try:
-        result = MODEL.transcribe(str(file_path))
+        audio.save(str(file_path))
 
-        return jsonify(result)
+        segments, info = MODEL.transcribe(str(file_path), beam_size=5)
+
+        full_text = "".join([segment.text for segment in segments]).strip()
+
+        return jsonify({
+            "text": full_text,
+            "language": info.language,
+            "language_probability": info.language_probability,
+            "duration": info.duration
+        })
     except Exception:
         import traceback
         app.logger.error(traceback.format_exc())
-        return jsonify({'message': 'Transcription error'}, 400)
+        return jsonify({'message': 'Transcription error'}), 400
     finally:
         if file_path.exists():
             file_path.unlink()
